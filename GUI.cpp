5e6a27357663b630b6e2708b484c35aebd7d9207
Amine.H <kilner99@gmail.com>
2024-12-19 20:54:38 -0500

    Fix: Implemented GUI obj.

--- GUI.cpp
+++ GUI.cpp
@@ -1,11 +1,46 @@
 #include "GUI.h"
-
+#include "iostream"
 GUI::GUI()
 {
+
+    std::cout << "Creating GUI object" << std::endl;
+    mainWindow.objectId = 1;
+    mainWindow.showWindow = true;
+    mainWindow.function = new demoWindowFunction();
+
+}
+
+GUI::GUI(GLFWwindow* windowApp)
+{
+
+    mainWindow.objectId = 1;
+    mainWindow.showWindow = true;
+    mainWindow.function = new demoWindowFunction();
 
 }
 
 GUI::~GUI()
 {
 
+}
+
+void GUI::createNewFrame()
+{
+    std::cout << "Creating new frame" << std::endl;
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+}
+
+void GUI::displayWindow(ImGuiIO& io)
+{
+    std::cout << "Calling mainWindow renderWindow function." << std::endl;
+    mainWindow.renderWindow(io);
+}
+
+void GUI::renderGUI()
+{
+    std::cout << "Im:Gui:: Render()" << std::endl;
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
--- GUI.h
+++ GUI.h
@@ -1,13 +1,19 @@
 #ifndef GUI_H
 #define GUI_H
 
+#include "GLFW/glfw3.h"
+#include "iostream"
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
 struct userInterfaceWindowFunction
 {
-	virtual void renderWindow(bool showWindow, int objectId)
+	virtual void renderWindow(bool showWindow, int objectId, ImGuiIO& io)
+	{
+
+	}
+	~userInterfaceWindowFunction()
 	{
 
 	}
@@ -17,21 +23,110 @@ struct userInterfaceWindow
 {
 	bool showWindow;
 	int objectId;
-	userInterfaceWindowFunction function;
+	
+	userInterfaceWindowFunction* function;
 	userInterfaceWindow* innerUserInterfaceWindows;
 
-	void renderWindow()
+	void renderWindow(ImGuiIO& io)
 	{
-		function.renderWindow(showWindow, objectId);
+		std::cout << "Inside userInterfaceWindow" << std::endl;
+		function->renderWindow(showWindow, objectId, io);
 	}
+
+	userInterfaceWindow()
+	{
+	}
+
+	~userInterfaceWindow()
+	{
+
+	}
+
 }; 
+
+struct renderObjectComponentWindowFunction : userInterfaceWindowFunction
+{
+	void renderWindow(bool showWindow, int objectId, ImGuiIO& io)
+	{
+
+	}
+};
+
+struct sceneSelectorWindowFunction : userInterfaceWindowFunction
+{
+	void renderWindow(bool showWindow, int objectId, ImGuiIO& io)
+	{
+
+	}
+};
+
+struct demoWindowFunction : userInterfaceWindowFunction
+{
+	void renderWindow(bool showWindow, int objectId, ImGuiIO& io)
+	{
+		std::cout << "Inside demoWindowFunction" << std::endl;
+		bool show_demo_window = true;
+		bool show_another_window = false;
+		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+		if (showWindow)
+		{
+			// 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
+			if (show_demo_window)
+				ImGui::ShowDemoWindow(&show_demo_window);
+
+			// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+			{
+				static float f = 0.0f;
+				static int counter = 0;
+
+				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+				ImGui::Checkbox("Another Window", &show_another_window);
+
+				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+					counter++;
+				ImGui::SameLine();
+				ImGui::Text("counter = %d", counter);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+				ImGui::End();
+			}
+
+			// 3. Show another simple window.
+			if (show_another_window)
+			{
+				ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
+				ImGui::Text("Hello from another window!");
+				if (ImGui::Button("Close Me"))
+					show_another_window = false;
+				ImGui::End();
+			}
+		}
+	}
+};
+
+
 class GUI
 {
 public:
 	GUI();
+	GUI(GLFWwindow* windowApp);
 	~GUI();
+	
+	void displayWindow(ImGuiIO& io);
+	void createNewFrame();
+	void renderGUI();
+
 private:
-	void renderMainWindow();
+	
+	userInterfaceWindow mainWindow;
 	
 };
+
 #endif
